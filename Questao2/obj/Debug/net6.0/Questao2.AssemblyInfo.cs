4e4d93fa2f37d0d9a56dec4166a32aaae6b76972
Leonardo Sousa Carvalho <leonardodont.dz@live.com>
2024-02-21 15:16:02 -0300

    aletrações

--- Questao2/obj/Debug/net6.0/Questao2.AssemblyInfo.cs
+++ Questao2/obj/Debug/net6.0/Questao2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Questao2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0031610d747497b4507ae6b33ebd941ddb3fad41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Questao2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Questao2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
